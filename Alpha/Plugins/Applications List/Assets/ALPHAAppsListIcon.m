587fbc81e2e76592efc5374c224366e636792a1f
Roman Gardukevich <roman@gardukevich.com>
2015-07-20 15:03:08 +0300

    Updated Applications list icon

--- Alpha/Plugins/Applications List/Assets/ALPHAAppsListIcon.m
+++ Alpha/Plugins/Applications List/Assets/ALPHAAppsListIcon.m
@@ -28,11 +28,35 @@ NSString *const ALPHAIconAppsListIdentifier = @"com.odnairy.alpha.icon.applicati
             [rectanglePath stroke];
             
             
-            //// Oval Drawing
-            UIBezierPath* ovalPath = [UIBezierPath bezierPathWithOvalInRect: CGRectMake(3, 3, 14, 14)];
+            //// Rectangle 2 Drawing
+            UIBezierPath* rectangle2Path = [UIBezierPath bezierPathWithRoundedRect: CGRectMake(4.5, 4.5, 4, 4) byRoundingCorners: UIRectCornerTopLeft cornerRadii: CGSizeMake(1, 1)];
+            [rectangle2Path closePath];
             [UIColor.lightGrayColor setStroke];
-            ovalPath.lineWidth = 2;
-            [ovalPath stroke];
+            rectangle2Path.lineWidth = 1;
+            [rectangle2Path stroke];
+            
+            
+            //// Rectangle 3 Drawing
+            UIBezierPath* rectangle3Path = [UIBezierPath bezierPathWithRoundedRect: CGRectMake(11.5, 4.5, 4, 4) cornerRadius: 1];
+            [UIColor.lightGrayColor setStroke];
+            rectangle3Path.lineWidth = 1;
+            [rectangle3Path stroke];
+            
+            
+            //// Rectangle 4 Drawing
+            UIBezierPath* rectangle4Path = [UIBezierPath bezierPathWithRect: CGRectMake(4.5, 11.5, 4, 4)];
+            [UIColor.lightGrayColor setStroke];
+            rectangle4Path.lineWidth = 1;
+            [rectangle4Path stroke];
+            
+            
+            //// Rectangle 5 Drawing
+            UIBezierPath* rectangle5Path = [UIBezierPath bezierPathWithRoundedRect: CGRectMake(11.5, 11.5, 4, 4) byRoundingCorners: UIRectCornerBottomRight cornerRadii: CGSizeMake(1, 1)];
+            [rectangle5Path closePath];
+            [UIColor.lightGrayColor setStroke];
+            rectangle5Path.lineWidth = 1;
+            [rectangle5Path stroke];
+
 
 
         };
