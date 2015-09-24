a25275d378d00117f1accd024ff370eda97ed9be
Dal Rupnik <legoless@gmail.com>
2015-09-24 13:34:34 +0200

    Updated Tap trigger to hook into launching notification

--- Alpha/Triggers/ALPHATapTrigger.m
+++ Alpha/Triggers/ALPHATapTrigger.m
@@ -10,6 +10,8 @@
 
 #import "ALPHATapTrigger.h"
 
+NSString *const ALPHATapTriggerKeyWindowChangeKeyPath = @"keyWindow";
+
 @interface ALPHATapTrigger ()
 
 @property (nonatomic, strong, readwrite) UILongPressGestureRecognizer *recognizer;
@@ -30,22 +32,101 @@
     return _recognizer;
 }
 
+- (void)dealloc
+{
+    //[[UIApplication sharedApplication] removeObserver:self forKeyPath:ALPHATapTriggerKeyWindowChangeKeyPath];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
 - (void)setEnabled:(BOOL)enabled
 {
     [super setEnabled:enabled];
     
     UIWindow *keyWindow = [ALPHATrigger keyWindow];
     
-    if (enabled)
+    if (!keyWindow)
     {
-        [keyWindow addGestureRecognizer:self.recognizer];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationNotification:) name:UIApplicationDidFinishLaunchingNotification object:nil];
+        //[[UIApplication sharedApplication] addObserver:self forKeyPath:ALPHATapTriggerKeyWindowChangeKeyPath options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:NULL];
     }
     else
     {
-        [keyWindow removeGestureRecognizer:self.recognizer];
-        
-        self.recognizer = nil;
+        if (enabled)
+        {
+            [keyWindow addGestureRecognizer:self.recognizer];
+        }
+        else
+        {
+            [keyWindow removeGestureRecognizer:self.recognizer];
+            
+            self.recognizer = nil;
+        }
     }
+}
+
+- (void)applicationNotification:(NSNotification *)notification
+{
+    if ([notification.name isEqualToString:UIApplicationDidFinishLaunchingNotification] && self.enabled)
+    {
+        [self updateTriggerWindow:[ALPHATrigger keyWindow]];
+    }
+}
+
+/*
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context
+{
+    if (![keyPath isEqualToString:ALPHATapTriggerKeyWindowChangeKeyPath])
+    {
+        return;
+    }
+    
+    id changedWindow = change[NSKeyValueChangeNewKey];
+    
+    //
+    // Ignore ALPHAWindow
+    //
+    if ([changedWindow isKindOfClass:NSClassFromString(@"ALPHAWindow")])
+    {
+        return;
+    }
+    else if ([changedWindow isKindOfClass:[UIWindow class]])
+    {
+        UIWindow* window = changedWindow;
+        
+        if (![window.gestureRecognizers containsObject:self.recognizer] && self.enabled)
+        {
+            [window addGestureRecognizer:self.recognizer];
+        }
+        
+        //
+        // Usually only do this once
+        //
+        [[UIApplication sharedApplication] removeObserver:self forKeyPath:ALPHATapTriggerKeyWindowChangeKeyPath];
+    }
+}
+*/
+
+- (void)updateTriggerWindow:(UIWindow *)window
+{
+    //
+    // Ignore ALPHAWindow
+    //
+    if ([window isKindOfClass:NSClassFromString(@"ALPHAWindow")])
+    {
+        return;
+    }
+    
+    if (![window.gestureRecognizers containsObject:self.recognizer] && self.enabled)
+    {
+        [window addGestureRecognizer:self.recognizer];
+    }
+        
+    //
+    // Usually only do this once
+    //
+    //[[UIApplication sharedApplication] removeObserver:self forKeyPath:ALPHATapTriggerKeyWindowChangeKeyPath];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+
 }
 
 @end
