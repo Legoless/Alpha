80cf73a1c58f595db4fab928a88c7424ceb6a008
Roman Gardukevich <roman@gardukevich.com>
2015-09-28 23:52:16 +0300

    Make offset on line drawing in DepthIndicatorView to draw full width line stroke.

--- Alpha/Plugins/View/Views/ALPHADepthIndicatorView.m
+++ Alpha/Plugins/View/Views/ALPHADepthIndicatorView.m
@@ -29,14 +29,22 @@
     
     [self.tintColor setStroke];
     
+    const CGFloat lineWidth = .5;
+    
+    // We need offset for line renderer to avoid halfline loss.
+    // In iOS system path stroking from the middle of path so the real line width will take
+    // coordinates: [x-lineWidth/2;x+lineWidth/2]. If we will stroke from x = 0 point
+    // the left part of line will lost.
+    CGFloat halfLineOffset = MAX(lineWidth/2,1/[UIScreen mainScreen].scale);
     for (NSUInteger i = 0; i < count; i++)
     {
-        CGFloat x = i * self.depthIndicatorWidth;
+        CGFloat x = i * self.depthIndicatorWidth+halfLineOffset;
         
         UIBezierPath *path = [UIBezierPath bezierPath];
         [path moveToPoint:CGPointMake(x, 0.0)];
         [path addLineToPoint:CGPointMake(x, rect.size.height)];
-        path.lineWidth = 0.5;
+        NSLog(@"Print path on coors: %f,%f",x*[UIScreen mainScreen].scale,rect.size.height*[UIScreen mainScreen].scale);
+        path.lineWidth = lineWidth;
         
         [path stroke];
     }
