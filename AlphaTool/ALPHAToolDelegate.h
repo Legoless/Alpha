9121c3c210898818e61cda9e68d127217217abbb
Dal Rupnik <legoless@gmail.com>
2016-12-12 14:07:22 +0100

    Added AlphaTool target

---
+++ AlphaTool/ALPHAToolDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ALPHAToolDelegate.h
+//  Alpha
+//
+//  Created by Dal Rupnik on 12/12/2016.
+//  Copyright © 2016 Unified Sense. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ALPHAToolDelegate: UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AlphaTool/ALPHAToolDelegate.m
@@ -0,0 +1,17 @@
+//
+//  ALPHAToolDelegate.m
+//  Alpha
+//
+//  Created by Dal Rupnik on 12/12/2016.
+//  Copyright © 2016 Unified Sense. All rights reserved.
+//
+
+#import "ALPHAToolDelegate.h"
+
+@implementation ALPHAToolDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    return YES;
+}
+
+@end
