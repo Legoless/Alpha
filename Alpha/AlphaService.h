52289217619c3802d5ba81bcb4f82ef3e08fe910
Dal Rupnik <legoless@gmail.com>
2016-12-13 17:06:48 +0100

    Added AlphaService target

---
+++ Alpha/AlphaService.h
@@ -0,0 +1,19 @@
+//
+//  AlphaService.h
+//  AlphaService
+//
+//  Created by Dal Rupnik on 13/12/2016.
+//  Copyright © 2016 Unified Sense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlphaService.
+FOUNDATION_EXPORT double AlphaServiceVersionNumber;
+
+//! Project version string for AlphaService.
+FOUNDATION_EXPORT const unsigned char AlphaServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlphaService/PublicHeader.h>
+
+
