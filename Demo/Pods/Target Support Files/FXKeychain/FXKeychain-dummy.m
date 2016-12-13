fac5f1a1d9ba728e0c2d6e801b39a0e99b16a296
Dal Rupnik <legoless@gmail.com>
2016-12-13 16:55:46 +0100

    Moved to demo

---
+++ Demo/Pods/Target Support Files/FXKeychain/FXKeychain-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FXKeychain : NSObject
+@end
+@implementation PodsDummy_FXKeychain
+@end
---
+++ Demo/Pods/Target Support Files/FXKeychain/FXKeychain-umbrella.h
@@ -0,0 +1,9 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "FXKeychain.h"
+
+FOUNDATION_EXPORT double FXKeychainVersionNumber;
+FOUNDATION_EXPORT const unsigned char FXKeychainVersionString[];
+
